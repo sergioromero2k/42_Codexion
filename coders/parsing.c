b870f65d8a8119eac1bcc85c404dd3ae92841a19
Sergio Alejandro <sergioromero2k@gmail.com>
2026-03-22 19:31:06 +0100

    day 01 finished

---
+++ coders/parsing.c
@@ -0,0 +1,71 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   parsing.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sergio-alejandro <sergio-alejandro@stud    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/03/22 05:23:17 by sergio-alej       #+#    #+#             */
+/*   Updated: 2026/03/22 19:30:19 by sergio-alej      ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "parsing.h"
+
+static int	is_valid_number(char *str)
+{
+	int	i;
+
+	i = 0;
+	if (!str[i])
+		return (0);
+	if (str[0] == '+')
+		i++;
+	if (!str[i])
+		return (0);
+	while (str[i])
+	{
+		if (str[i] < '0' || str[i] > '9')
+			return (0);
+		i++;
+	}
+	return (1);
+}
+
+int	parse_config_2(t_config *config, char *str)
+{
+	if (strcmp(str, "fifo") == 0)
+		config->scheduler = SCHED_FIFO;
+	else if (strcmp(str, "edf") == 0)
+		config->scheduler = SCHED_EDF;
+	else
+		return (write(2, "Error: Scheduler must be 'fifo' or 'edf'\n", 41), 1);
+	return (0);
+}
+
+int	parse_config(t_config *config, int argc, char **argv)
+{
+	int	i;
+
+	if (argc != 9)
+		return (write(2, "Error: Wrong number of arguments\n", 33), 1);
+	i = 1;
+	while (i <= 7)
+	{
+		if (!is_valid_number(argv[i]))
+			return (write(2,
+					"Error: Invalid argument \
+					%d (must be positive integer)\n", 15), 1);
+		i++;
+	}
+	config->number_of_coders = atoi(argv[1]);
+	config->time_to_burnout = atol(argv[2]);
+	config->time_to_compile = atol(argv[3]);
+	config->time_to_debug = atol(argv[4]);
+	config->time_to_refactor = atol(argv[5]);
+	config->number_of_compiles_required = atol(argv[6]);
+	config->dongle_cooldown = atol(argv[7]);
+	if (config->number_of_coders < 1)
+		return (write(2, "Error: Coder count\n", 19), 1);
+	return (parse_config_2(config, argv[8]));
+}
--- coders/parsing.h
+++ coders/parsing.h
@@ -6,30 +6,40 @@
 /*   By: sergio-alejandro <sergio-alejandro@stud    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/21 20:47:22 by sergio-alej       #+#    #+#             */
-/*   Updated: 2026/03/21 20:56:25 by sergio-alej      ###   ########.fr       */
+/*   Updated: 2026/03/22 19:29:16 by sergio-alej      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef PARSING_H
+# define PARSING_H
 
-# ifndef    PARSING_H
-# define    PARSING_H
+#include <stdint.h>
+#include <stdio.h>
+#include <limits.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <unistd.h>
+#include <stdarg.h>
+#include <string.h>
 
+typedef enum s_schedule
+{
+	SCHED_FIFO,
+	SCHED_EDF
+}				t_schedule;
 
-typedef enum s_schedule {
-    SCHED_FIFO,
-    SCHED_EDF
-} t_schedule;
+typedef struct s_config
+{
+	int			number_of_coders;
+	long		time_to_burnout;
+	long		time_to_compile;
+	long		time_to_debug;
+	long		time_to_refactor;
+	long		number_of_compiles_required;
+	long		dongle_cooldown;
+	t_schedule	scheduler;
+}				t_config;
 
+int				parse_config(t_config *config, int argc, char **argv);
 
-typedef struct s_config {
-    int number_of_coders;
-    int time_to_burnout;
-    int time_to_compile;
-    int time_to_debug;
-    int time_to_refactor;
-    int number_of_compiles_required;
-    int dongle_cooldown;
-    t_schedule scheduler;
-} t_config;
-
-# endif
+#endif
