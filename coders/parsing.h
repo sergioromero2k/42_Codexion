c127de2a4238bce220838bb9c62e815e35ce9e97
Sergio Alejandro <sergioromero2k@gmail.com>
2026-03-22 05:17:36 +0100

    day 1 almost finished

--- coders/parsing.h
+++ coders/parsing.h
@@ -1,21 +1,35 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   main.c                                             :+:      :+:    :+:   */
+/*   parsing.h                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: sergio-alejandro <sergio-alejandro@stud    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2026/03/21 17:33:52 by sergio-alej       #+#    #+#             */
-/*   Updated: 2026/03/21 20:46:45 by sergio-alej      ###   ########.fr       */
+/*   Created: 2026/03/21 20:47:22 by sergio-alej       #+#    #+#             */
+/*   Updated: 2026/03/21 20:56:25 by sergio-alej      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stdio.h>
+
+# ifndef    PARSING_H
+# define    PARSING_H
 
 
-enum schedule {
+typedef enum s_schedule {
     SCHED_FIFO,
     SCHED_EDF
-};
+} t_schedule;
 
 
+typedef struct s_config {
+    int number_of_coders;
+    int time_to_burnout;
+    int time_to_compile;
+    int time_to_debug;
+    int time_to_refactor;
+    int number_of_compiles_required;
+    int dongle_cooldown;
+    t_schedule scheduler;
+} t_config;
+
+# endif
